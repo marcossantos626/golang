0761c95c4a558e9e36bfc07d0ccf71d4fbf3715b
Marcos Santos <marcossantos.sysmap@natura.net>
2021-10-14 13:39:41 -0300

    aula

---
+++ aulas/s5-tipos/ex060-pseudoheranca/pseudoheranca.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type carro struct {
+	nome       string
+	velocidade int
+}
+
+type ferrari struct {
+	carro       //campos anonimos
+	turboLigado bool
+}
+
+func main() {
+	f := ferrari{}
+	f.nome = "F40"
+	f.velocidade = 0
+	f.turboLigado = true
+
+	fmt.Printf("A ferrari %s está com o turbo ligado? %v\n", f.nome, f.turboLigado)
+	fmt.Println(f)
+}
